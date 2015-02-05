1d2386864230e3a14f021fb1fc83519cd1de65ae
BeanYoung <guobasanxian@gmail.com>
2015-02-05 22:11:25 +0800

    add init_beanstalk

--- beanstalkw/worker.py
+++ beanstalkw/worker.py
@@ -17,16 +17,7 @@ class BaseWorker(object):
     def work(self):
         while True:
             try:
-                if self.beanstalk:
-                    self.beanstalk.reconnect()
-                else:
-                    self.beanstalk = \
-                        beanstalkc.Connection(self.host, self.port)
-                for tube in self.tubes:
-                    self.beanstalk.watch(tube)
-                for tube in self.beanstalk.watching():
-                    if tube not in self.tubes:
-                        self.beanstalk.ignore(tube)
+                self.init_beanstalk()
 
                 while True:
                     job = self.reserve(timeout=10)
@@ -39,11 +30,19 @@ class BaseWorker(object):
                     except Exception, e:
                         job.bury()
             except beanstalkc.SocketError, e:
-                time.sleep(2)
-                if self.beanstalk:
-                    self.beanstalk.close()
-                self.beanstalk = None
+                pass
             time.sleep(2)
+
+    def init_beanstalk(self):
+        if self.beanstalk:
+            self.beanstalk.close()
+        self.beanstalk = \
+            beanstalkc.Connection(self.host, self.port)
+        for tube in self.tubes:
+            self.beanstalk.watch(tube)
+        for tube in self.beanstalk.watching():
+            if tube not in self.tubes:
+                self.beanstalk.ignore(tube)
 
     def reserve(self, timeout):
         pass
@@ -58,9 +57,15 @@ class Worker(BaseWorker):
 
 
 class FailedWorker(BaseWorker):
+    def __init__(self, host, port, tubes):
+        super(FailedWorker, self).__init__(host, port, tubes)
+        self.tube_index = 0
+
     def reserve(self, timeout):
         for i in range(timeout):
+            self.beanstalk.use(self.tubes[self.tube_index % len(self.tubes)])
             job = self.beanstalk.peek_buried()
             if job:
                 return job
+            self.tube_index += 1
             time.sleep(1)
